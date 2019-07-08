fa608856d96abcdd423372fabc7c4f178a918a3d
jiokeokwuosa <okwuosachijioke@gmail.com>
2019-07-08 05:25:41 +0100

    feature(validate-property):validate property information [Finishes #167119168]

--- API/src/routes/propertyRoute.js
+++ API/src/routes/propertyRoute.js
@@ -2,9 +2,17 @@ import { Router } from 'express';
 import propertyController from '../controllers/propertyController';
 import verifyToken from '../middlewares/verifyToken';
 import upload from '../middlewares/image-upload';
+import PostProperty from '../middlewares/propertyValidation';
 
 const router = Router();
 
-router.post('/', verifyToken, upload, propertyController.postProperty);
+router.post(
+  '/',
+  verifyToken,
+  upload,
+  PostProperty.validate(),
+  PostProperty.verifyValidationResult,
+  propertyController.postProperty
+);
 
 export default router;
